2ccc2e10b42cc06ec889af4213c6592a25142393
navgit <mail.navazish@gmail.com>
2018-06-26 03:31:46 +0000

    pushing code

--- s3.tf
+++ s3.tf
@@ -1,22 +1,25 @@
-resource "aws_s3_bucket" "b" {
-    bucket = "mybucket-462281"
-    acl = "public-read"
- 
-    tags {
-        Name = "mybucket-462281"
-    }
-    policy = <<POLICY
+resource "aws_s3_bucket" "webbuck" {
+  provider = "aws.prod"
+
+  bucket = "mybucket-462281"
+  acl = "public-read"
+  policy = <<POLICY
 {
-            
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow", 
-            "Principal": "*", 
-            "Action": "s3:GetObject", 
-            "Resource": "arn:aws:s3:::mybucket-462281/*" 
-        } 
-    ] 
+  "Version":"2012-10-17",
+  "Statement":[{
+    "Sid":"PublicReadForGetBucketObjects",
+        "Effect":"Allow",
+      "Principal": "*",
+      "Action":"s3:GetObject",
+      "Resource":["arn:aws:s3:::mybucket-462281/*"
+      ]
+    }
+  ]
 }
-POLICY            
+POLICY
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
 }
